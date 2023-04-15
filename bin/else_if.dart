57b68fa33f18090cbe5fc9ce7063edf21827d652
kazem <kazem.codes@gmail.com>
2023-04-15 13:52:44 +0330

    update

---
+++ bin/else_if.dart
@@ -0,0 +1,23 @@
+
+void main() {
+
+
+  int x = 1;
+  int z = 6;
+  // if(x > 2) {
+  //   print("x is greater than two");
+  // } else {
+  //   print("x is less than two");
+  // }
+  // if ( x > 2 && z < 8 || x > 0) {
+  //   print("Shart 1");
+  // } else if(x > 4) {
+  //   print("Shart 2");
+  // } else {
+  //   print("Shart 3");
+  // }
+  String message = (x > 0) ? "x is greater than 0" : "x is less than 0";
+
+  print(message);
+
+}
